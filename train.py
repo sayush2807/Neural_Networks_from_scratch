f35ce20d804bed8eafbfdffb181beb0b7a143286
Ayush Srivastava <sayush2807@gmail.com>
2026-02-11 21:15:45 -0800

    Added Gradient Norm Check

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from losses.mse import mse_loss
 from optimizer.SGD import SGD
+import numpy as np
 
 def train(linear, x,y, epochs, lr):
     optimizer = SGD(params = linear.parameters(), lr = lr )
@@ -7,4 +8,8 @@ def train(linear, x,y, epochs, lr):
         y_hat = linear.forward(x)
         print("Epoch ", i+1, " : MSE Loss", mse_loss(y,y_hat))
         linear.backward(y,y_hat)
+        linear.backward(y, y_hat)
+        #gradient norm check -- these should be non-zero and reasonable
+        print("||dW|| =", np.linalg.norm(linear.gradient_w))
+        print("||db|| =", np.linalg.norm(linear.gradient_b))
         optimizer.step()
