ae813728ab9275b7e3dd04fc54fb7d623d2f3c0a
Ayush Srivastava <sayush2807@gmail.com>
2026-02-15 21:07:38 -0800

    Update sigmoid.py

--- layers/sigmoid.py
+++ layers/sigmoid.py
@@ -1,5 +1,11 @@
 import numpy as np
 
 class sigmoid:
-    def __init__():
-        
+    def __init__(self,z):
+        self.y_hat = None
+
+    def forward(z):
+        self.y_hat = i/(1+np.exp(-z))
+        return y_hat
+
+    def backward():
