6054442bfd17bdf68896740090c1130175ffb117
Ayush Srivastava <sayush2807@gmail.com>
2026-02-15 21:43:50 -0800

    Update train.py

--- train.py
+++ train.py
@@ -2,13 +2,13 @@ from losses.mse import mse_loss
 from optimizer.SGD import SGD
 import numpy as np
 
-def train(linear, x,y, epochs, lr):
+def train(linear, x,y, epochs, lr, loss):
     optimizer = SGD(params = linear.parameters(), lr = lr )
     for i in range(epochs):
         y_hat = linear.forward(x)
-        print("Epoch ", i+1, " : MSE Loss", mse_loss(y,y_hat))
+        print("Epoch ", i+1, " : Loss", loss(y,y_hat))
         linear.backward(y,y_hat)
-        linear.backward(y, y_hat)
+        #linear.backward(y, y_hat)
         #gradient norm check -- these should be non-zero and reasonable
         print("||dW|| =", np.linalg.norm(linear.gradient_w))
         print("||db|| =", np.linalg.norm(linear.gradient_b))
