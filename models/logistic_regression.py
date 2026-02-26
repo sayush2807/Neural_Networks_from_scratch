b04e15876acbb504b36177abd7d6c8bfd0b83e99
Ayush Srivastava <sayush2807@gmail.com>
2026-02-25 23:07:37 -0800

    Update logistic_regression.py

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -8,5 +8,8 @@ class LogisticRegressionClass:
     def forward(self, x):
         self.linear.forward()
         self.sigmoid.forward()
-        self.bce()
-        self.sigmoid.backward()
+        self.bce.forward()
+        self.bce.backward()# computes gradient of loss wrt y_hat
+        self.sigmoid.backward() # this calculates chain rule's next part
+        self.linear.backward() #this calculates chain rule'next part
+        self.optimizer.step() # think of this , first declare optimizer then how would sigmoid.forward/backward look? will it be optimizer.forward/backward 
