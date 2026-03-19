b299c16dc2bb31712bdd7fd24eb5c3ceee2ea07d
Ayush Srivastava <sayush2807@gmail.com>
2026-03-18 22:21:12 -0500

    Update logistic_regression.py

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,15 +1,20 @@
-from layers.linear import Dense, sigmoid
+from layers.linear import Dense
+from layers.sigmoid import Sigmoid
+
+
 class LogisticRegressionClass:
     def __init__(self, n_features):
         self.linear = Dense(n_features, 1)
+        self.sigmoid = Sigmoid()
         
 
     def forward(self, x):
-        self.linear.forward(x)
-        self.sigmoid.forward()
-        self.bce.forward()
+        z = self.linear.forward(x)
+        a = self.sigmoid.forward(z)
+        return a
         
-    def backward(self):
-        self.bce.backward()
-        self.sigmoid.backward()
-        self.linear.backwar()
+    def backward(self, da):
+        dz = self.sigmoid.backward(da)
+        dx = self.linear.backward(dz)
+        return dx
+    
