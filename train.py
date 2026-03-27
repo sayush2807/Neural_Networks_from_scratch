4b089279acea23d909da47a2b83808edbf5c011c
Ayush Srivastava <sayush2807@gmail.com>
2026-03-26 23:52:04 -0500

    Update train.py

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ def train(linear, x,y, epochs, lr, loss):
         y_hat = linear.forward(x)
         print("Epoch ", i+1, " : Loss", loss(y,y_hat))
         linear.backward(y,y_hat)
-        #linear.backward(y, y_hat)
+        
         #gradient norm check -- these should be non-zero and reasonable
         print("||dW|| =", np.linalg.norm(linear.gradient_w))
         print("||db|| =", np.linalg.norm(linear.gradient_b))
