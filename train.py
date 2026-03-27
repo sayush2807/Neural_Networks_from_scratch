f944d1decd7d16537cda9dcf0b65b90d62b4e86b
Ayush Srivastava <sayush2807@gmail.com>
2026-03-27 18:54:41 -0500

    Update BCELoss.py

--- train.py
+++ train.py
@@ -1,19 +1,21 @@
-from losses.mse import bce_loss
+from losses.bce import bce_loss
 from optimizer.SGD import SGD
 import numpy as np
-from models import LogisticRegression
+from models.logistic_regression import LogisticRegression
 # train logistic regression
 
 def train(linear, x,y, epochs, lr, loss):
-    logistic_regressor = LogisticRegression()
+    model = LogisticRegression(n_features = x.shape[1])
+    loss_fn = BCELoss()
     optimizer = SGD(params = linear.parameters(), lr = lr )
     sigmoid = Sigmoid()
     for i in range(epochs):
-        y_hat = linear.forward(x)
-        print("Epoch ", i+1, " : Loss", loss(y,y_hat))
-        linear.backward(y,y_hat)
-        
+        y_hat = model.forward(x)
+        loss = loss_fn(y,y_hat)
+        da = loss_fn.backward(y,y_hat)
+        model.backward(da)
+        optimizer.step()
+        print("Epoch ", i+1, " : Loss", loss)
         #gradient norm check -- these should be non-zero and reasonable
         print("||dW|| =", np.linalg.norm(linear.gradient_w))
         print("||db|| =", np.linalg.norm(linear.gradient_b))
-        optimizer.step()
