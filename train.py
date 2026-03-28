9fc20cf3bf2d7a6e527d8ea783480204f1a7e51c
Ayush Srivastava <sayush2807@gmail.com>
2026-03-27 20:17:20 -0500

    Update train.py

--- train.py
+++ train.py
@@ -4,14 +4,13 @@ import numpy as np
 from models.logistic_regression import LogisticRegression
 # train logistic regression
 
-def train(linear, x,y, epochs, lr, loss):
+def train(x,y, epochs, lr, loss):
     model = LogisticRegression(n_features = x.shape[1])
     loss_fn = BCELoss()
     optimizer = SGD(params = linear.parameters(), lr = lr )
-    sigmoid = Sigmoid()
     for i in range(epochs):
         y_hat = model.forward(x)
-        loss = loss_fn(y,y_hat)
+        loss = loss_fn.forward(y,y_hat)
         da = loss_fn.backward(y,y_hat)
         model.backward(da)
         optimizer.step()
